3995eb5dcf3207a96e8300a028aecce6a6ddcc42
Alessio <Alessio@MSI>
2022-11-10 19:31:11 +0100

    Aggiungere i file di progetto.

---
+++ GiorniDelMese/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+string Mese;
+
+int Giorni;
+
+Console.WriteLine("Inserisci il mese: ");
+Mese=Console.ReadLine();
+
+if (Mese == "Gennaio" || Mese ==  "Marzo" || Mese == "Maggio" || Mese == "Luglio" || Mese == "Agosto" || Mese == "Ottobre" || Mese == "Dicembre")
+    {
+    Giorni = 31;
+    Console.Write("Questo mese ha ");
+    Console.Write(Giorni);
+    Console.Write(" Giorni");
+}
+else
+{
+    if (Mese == "Aprile" || Mese == "Giugno")
+}
